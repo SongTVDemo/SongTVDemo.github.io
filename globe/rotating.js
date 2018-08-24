52d04ef3f6d493dc9f15b7296e52e60aaa129ac9
SongTVDemo <mjmccolgan2@gmail.com>
2018-08-23 21:58:30 -0700

    scale fix rotating.js

--- globe/rotating.js
+++ globe/rotating.js
@@ -35,7 +35,7 @@
   // The `zoom` and `drag` plugins enable
   // manipulating the globe with the mouse.
   globe.loadPlugin(planetaryjs.plugins.zoom({
-    scaleExtent: [100, 800]
+    scaleExtent: [100, 600]
   }));
   globe.loadPlugin(planetaryjs.plugins.drag({
     // Dragging the globe should pause the
